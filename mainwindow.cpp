ba653593c29f34e1ebfa5083b71f623549b2c594
Antonio Samuel <asamuel@vectron.com>
2016-12-20 15:17:36 -0500

    adding qt project for qt fourm

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "libMPSSE_i2c.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    Init_libMPSSE();
+}
